e49bb7c2907b8580ecc83e9045dcef2eb1d92eb7
Bezlimix <72493579+bezlimix@users.noreply.github.com>
2021-12-20 22:25:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,33 +15,33 @@ document.getElementById('test1').onclick = function () {
     document.getElementById('test').classList.remove('link-clicked');
 }
 
-document.addEventListener('DOMContentLoaded', function(){
-    const form = document.getElementById('form');
-    form.addEventListener('submit', formSend);
+// document.addEventListener('DOMContentLoaded', function(){
+//     const form = document.getElementById('form');
+//     form.addEventListener('submit', formSend);
 
-    async function formSend(e){
-        e.preventDefault();
+//     async function formSend(e){
+//         e.preventDefault();
 
-        let error = 0;
+//         let error = 0;
 
-        let formData = new FormData(form);
+//         let formData = new FormData(form);
 
-        if(error===0){
-            form.classList.add('_sending');
-            let response = await fetch('sendmail.php', {
-                method: 'POST',
-                body:formData
-            });
-            if(!response.ok){
-                 let result = await response.json();
-                alert(result.message);
-                form.reset();
-                alert('asd');
-            }
-            else{
-                alert('ошибка');
-            }
+//         if(error===0){
+//             form.classList.add('_sending');
+//             let response = await fetch('sendmail.php', {
+//                 method: 'POST',
+//                 body:formData
+//             });
+//             if(!response.ok){
+//                  let result = await response.json();
+//                 alert(result.message);
+//                 form.reset();
+//                 alert('asd');
+//             }
+//             else{
+//                 alert('ошибка');
+//             }
 
-        }
-    }
-})
+//         }
+//     }
+// })
